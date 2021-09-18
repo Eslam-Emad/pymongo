84c4b819076fa465e8993896f4e68fc2298c03d3
Eslam-Emad <Islam-Emad@outlook.com>
2021-09-18 18:39:44 +0200

    first commit

--- todo/config/db.py
+++ todo/config/db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://eslam:eslam@todocluster.tdbat.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://<username>:<password>@todocluster.tdbat.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client.todo_application
 collection_name = db['todo_app']
 
